a2d08af9114b3b8d8b0ef7ddb1e966508d30fd0b
Howard Yin <hyin@WTLDEV.NET>
2017-03-23 18:35:30 -0400

    Added icons

--- app/src/boot/boot.ts
+++ app/src/boot/boot.ts
@@ -20,12 +20,12 @@ export const angularApp = angular.module('MaterialStart', ['ngMaterial', 'ngSani
     // Register the user `avatar` icons
     $mdIconProvider
       .defaultIconSet("./assets/svg/avatars.svg", 128)
-      .icon("menu", "../assets/svg/menu.svg", 24)
-      .icon("share", "../assets/svg/share.svg", 24)
-      .icon("google_plus", "../assets/svg/google_plus.svg", 24)
-      .icon("hangouts", "../assets/svg/hangouts.svg", 24)
-      .icon("twitter", "../assets/svg/twitter.svg", 24)
-      .icon("phone", "../assets/svg/phone.svg", 24);
+      .icon("menu", "./assets/svg/menu.svg", 24)
+      .icon("share", "./assets/svg/share.svg", 24)
+      .icon("google_plus", "./assets/svg/google_plus.svg", 24)
+      .icon("hangouts", "./assets/svg/hangouts.svg", 24)
+      .icon("twitter", "./assets/svg/twitter.svg", 24)
+      .icon("phone", "./assets/svg/phone.svg", 24);
 
     $mdThemingProvider.theme('default')
       .primaryPalette('brown')
